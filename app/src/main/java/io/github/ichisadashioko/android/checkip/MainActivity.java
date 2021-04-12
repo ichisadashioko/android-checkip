28e8922a09df02567ec47eb7d91c0f01950688e9
shioko <ichisadashioko@gmail.com>
2021-04-12 05:16:38 +0000

    hide action bar and status bar

--- app/src/main/java/io/github/ichisadashioko/android/checkip/MainActivity.java
+++ app/src/main/java/io/github/ichisadashioko/android/checkip/MainActivity.java
@@ -1,9 +1,12 @@
 package io.github.ichisadashioko.android.checkip;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +15,22 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow()
+                    .setFlags(
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the status bar is hidden, so
+            // hide that too if necessary.
+            ActionBar actionBar = getActionBar();
+            actionBar.hide();
+        }
+
         setContentView(R.layout.activity_main);
 
         ipView = findViewById(R.id.ipview);
@@ -20,7 +39,7 @@ public class MainActivity extends Activity {
     public void onIpViewClicked(View v) {
         if (ipView.statusColor == Color.BLACK) {
             // TODO initialize request to external server to get our current IP
-            ipView.changeStatusColor(Color.YELLOW);
+            ipView.changeStatusColor(Color.BLUE);
             (new QueryIPThread(this)).start();
         } else if ((ipView.statusColor == Color.GREEN) && (!ipView.showReponse)) {
             ipView.showReponse = true;
